f6eee540fcfd06cb31034e5924eceb1a9c740078
Your Name <you@example.com>
2025-05-12 01:35:27 +0300

    vercel deploy

--- Subscribers form API/api/server.js
+++ Subscribers form API/api/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import serverless from 'serverless-http';
 import subscriberRoutes from './routes/subscribersRoute.js'
 
 dotenv.config()
@@ -21,3 +22,7 @@ app.use('/api', subscriberRoutes)
 app.listen(5000, () => {
   console.log(`Server running on http://localhost:${5000}`)
 })
+
+
+
+export const handler = serverless(app);
